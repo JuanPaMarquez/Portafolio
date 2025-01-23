900de23a61244abd8593270ec6f4749ecdf09356
Juan Pablo Marquez Sanchez <juan013sanchez@gmail.com>
2025-01-23 12:52:45 -0500

    se mejoró la estructura del portafolio, añadiendo información y enlaces a GitHub para cada proyecto, y se optimizó el botón de copia del correo electrónico

--- index.js
+++ index.js
@@ -6,17 +6,43 @@ proyectos.forEach(proyecto => {
   const div = document.createElement("div");
   div.classList.add("proyect");
 
+  const imgInfo = document.createElement("div");
+  imgInfo.classList.add("img-info");
+
   const img = document.createElement("img");
   img.src = proyecto.img;
+
+  const barraBotones = document.createElement("div")
+  barraBotones.classList.add("barra-botones")
+
+  const infoProject = document.createElement("div");
+  infoProject.classList.add("info-project")
+
+  const infoTitulo = document.createElement("h3")
+  infoTitulo.innerText = proyecto.titulo;
+  const infoTexto = document.createElement("p")
+  infoTexto.innerText = proyecto.info;
+  infoProject.appendChild(infoTitulo)
+  infoProject.appendChild(infoTexto)
+
+  const gitButton = document.createElement("a");
+  gitButton.classList.add("github");
+  gitButton.href = proyecto.github;
+  gitButton.target = "_blank"
+  gitButton.innerText = "GitHub"
 
   const a = document.createElement("a");
   a.classList.add("project-tile");
   a.href = proyecto.link;
-  a.innerText = proyecto.titulo;
+  a.innerText = "Ver Proyecto";
   a.target = "_blank";
   
-  div.appendChild(img);
-  div.appendChild(a);
+  imgInfo.appendChild(img);
+  imgInfo.appendChild(infoProject);
+  div.appendChild(imgInfo);
+  barraBotones.appendChild(gitButton);
+  barraBotones.appendChild(a);
+  div.appendChild(barraBotones)
   contenedor.appendChild(div);
 })
 
@@ -51,3 +77,6 @@ async function copyToClipboard() {
     console.error("Error al copiar el correo electrónico", err);
   }
 }
+
+const copy = document.getElementById("copy");
+copy.addEventListener("click", copyToClipboard);
